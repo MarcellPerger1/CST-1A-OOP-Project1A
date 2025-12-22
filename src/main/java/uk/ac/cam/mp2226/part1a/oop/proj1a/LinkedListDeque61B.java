ff66a6f1835c7e392f2ff697feefcba402a52e2d
Marcell Perger <marcell.perger.1@gmail.com>
2025-12-22 23:31:02 +0000

    Add skeleton files

---
+++ src/main/java/uk/ac/cam/mp2226/part1a/oop/proj1a/LinkedListDeque61B.java
@@ -0,0 +1,50 @@
+package uk.ac.cam.mp2226.part1a.oop.proj1a;
+
+import java.util.List;
+
+public class LinkedListDeque61B<T> implements Deque61B<T> {
+    @Override
+    public void addFirst(T x) {
+
+    }
+
+    @Override
+    public void addLast(T x) {
+
+    }
+
+    @Override
+    public List<T> toList() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
+}
